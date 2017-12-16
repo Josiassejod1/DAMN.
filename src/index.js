5486e789f2d70cc94fa69a6c546aba5968db6533
Dalvin Josias <dalvin.josias@gmail.com>
2017-12-16 15:27:31 -0500

    Utilized Map to add keys to list of elements.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+var tracks = ['BLOOD', 
+          'DNA',
+          'YAH',
+          'ELEMENT',
+          'LOYALTY',
+          'PRIDE',
+          'HUMBLE',
+          'LUST',
+          'XXX',
+          'FEAR',
+          'GOD',
+          'DUCKWORTH'];
+const songList = tracks.map((list, i) =>
+    <li key={"track_" + i}>{list}</li>);
 
-const damn =  (
-  <ol>
-    <li>BLOOD</li>
-    <li>DNA</li>
-    <li>YAH</li>
-    <li>ELEMENT</li>
-    <li>FEEL</li>
-    <li>LOYALTY</li>
-    <li>PRIDE</li>
-    <li>HUMBLE</li>
-    <li>LUST</li>
-    <li>LOVE</li>
-    <li>XXX</li>
-    <li>FEAR</li>
-    <li>GOD</li>
-    <li>DUCKWORTH</li>
-  </ol>
-);
+const album =  <ol>{songList}</ol>
 
-render(damn, document.getElementById('root'));
+
+render(album, document.getElementById('root'));
