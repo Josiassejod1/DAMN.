9bdf533af6c93eb7e7c66faa16958dd61099890b
Dalvin Josias <dalvin.josias@gmail.com>
2017-12-16 13:27:22 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const damn =  (
+  <ol>
+    <li>BLOOD</li>
+    <li>DNA</li>
+    <li>YAH</li>
+    <li>ELEMENT</li>
+    <li>FEEL</li>
+    <li>LOYALTY</li>
+    <li>PRIDE</li>
+    <li>HUMBLE</li>
+    <li>LUST</li>
+    <li>LOVE</li>
+    <li>XXX</li>
+    <li>FEAR</li>
+    <li>GOD</li>
+    <li>DUCKWORTH</li>
+  </ol>
+);
+
+render(damn, document.getElementById('root'));
