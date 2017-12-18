2181723fa07af666152128d1a58ac450dae11355
Dalvin Josias <dalvin.josias@gmail.com>
2017-12-17 23:48:51 -0500

    Learning how to nest react components.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,59 @@
 import React from 'react';
 import { render } from 'react-dom';
-var tracks = ['BLOOD', 
-          'DNA',
-          'YAH',
-          'ELEMENT',
-          'LOYALTY',
-          'PRIDE',
-          'HUMBLE',
-          'LUST',
-          'XXX',
-          'FEAR',
-          'GOD',
-          'DUCKWORTH'];
-const songList = tracks.map((list, i) =>
-    <li key={"track_" + i}>{list}</li>);
+import Helmet from 'react-helmet';
 
-const album =  <ol>{songList}</ol>
+
+
+const coverStyle = {
+  display: 'block',
+  margin: 'auto'
+};
+
+const listStyle = {
+  'text-align':'center',
+  'list-style':'none'
+}
+
+var tracks = ['BLOOD.', 
+          'DNA.',
+          'YAH.',
+          'ELEMENT.',
+          'LOYALTY.',
+          'PRIDE.',
+          'HUMBLE.',
+          'LUST.',
+          'XXX.',
+          'FEAR.',
+          'GOD.',
+          'DUCKWORTH.'];
+
+//This keeps track of the list order
+const songList = tracks.map((list, i) =>
+    <li style= {listStyle}
+    key={"track_" + i}>{list}</li>);
+
+const album =  <ul>{songList}</ul>
+
+//Kendrick 
+var kendrick = {
+  src: "http://s3.amazonaws.com/hiphopdx-production/2017/04/Kendrick-Lamar-DAMN-album-cover-featured-827x620.jpg",
+  size: "150px"
+}
+
+class Cover extends React.Component {
+  render() {
+    return (
+      <img 
+      src={kendrick.src}
+      width={kendrick.size}
+      height={kendrick.size}
+      style={coverStyle}
+      />
+    );
+  }
+}
+
+
 
 class Artist extends React.Component {
   render() {
@@ -23,5 +61,18 @@ class Artist extends React.Component {
   }
 }
 
+class Album extends React.Component {
+  render() {
+    return (
 
-render(<Artist/>, document.getElementById('root'));
+
+    <div>
+      <Cover/>
+      <Artist/>
+    </div>
+    );
+  }
+}
+
+
+render(<Album/>, document.getElementById('root'));
